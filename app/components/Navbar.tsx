887a372473ab0dfa85b941b77284867dca1de790
Anshul Garg <anshulgarg398@gmail.com>
2023-10-30 14:42:05 +0530

    some changes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -33,18 +33,7 @@ export default function Navbar() {
                     }`}
                   >
                     Home
-                  </Link>
-                  <Link
-                    href="/guestbook"
-                    prefetch
-                    className={`${
-                      pathname === "/guestbook"
-                        ? "border-teal-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                        : "border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                    }`}
-                  >
-                    Guestbook
-                  </Link>
+                  </Link>              
                   <Link
                     href="/projects"
                     prefetch
@@ -111,17 +100,6 @@ export default function Navbar() {
                 } `}
               >
                 Home
-              </Link>
-              <Link
-                href="/guestbook"
-                prefetch
-                className={`${
-                  pathname == "/guestbook"
-                    ? "bg-teal-50 border-teal-500 text-teal-500 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
-                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-teal-500 block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"
-                } `}
-              >
-                Guestbook
               </Link>
               <Link
                 href="/projects"
